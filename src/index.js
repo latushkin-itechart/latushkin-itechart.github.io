cfcf4157caf7c6377221def26fcd55c8a28b55f4
Denis Latushkin <latushkin.itechart@outlook.com>
2020-08-24 17:57:32 +0300

    SW base

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 window.navigator.serviceWorker.register('./src/sw.js').then((registration) => {
-    console.log('SW registered!', registration);
+    console.log('Index: SW registered!', registration);
+
+    window.addEventListener('_unload', () => {
+        registration.unregister().then(() => {
+            console.log('SW unregistered!')
+        })
+    })
 })
--- src/sw.js
+++ src/sw.js
@@ -1,7 +1,11 @@
 self.addEventListener('install', (event) => {
-    console.log('SW Installed!', event)
+    console.log('SW: SW Installed!', event)
 });
 
 self.addEventListener('activate', (event) => {
-    console.log('SW Activated!', event);
+    console.log('SW: SW Activated!', event);
+})
+
+self.addEventListener('fetch', (event) => {
+    console.log('SW: Fetch caught!', event);
 })
